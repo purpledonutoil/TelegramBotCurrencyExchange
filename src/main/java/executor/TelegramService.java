fc055e1c321241c39ef8f84c0adaa50caea64458
purpledonutoil <purpledonutoil@gmail.com>
2025-04-03 12:59:38 +0200

    configuration updated

--- src/main/java/executor/TelegramService.java
+++ src/main/java/executor/TelegramService.java
@@ -1,22 +1,57 @@
 package executor;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class TelegramService {
-    private static String botToken;
+import java.util.Map;
+
+public class TelegramService extends TelegramLongPollingBot implements TelegramBot {
     private static String botName;
 
-    TelegramService(){
-        Dotenv dotenv = Dotenv.load();
-        botToken = dotenv.get("BOT_TOKEN");
-        botName = dotenv.get("BOT_NAME");
-    }
-
-    public static String getBotName() {
+    @Override
+    public String getBotUsername() {
         return botName;
     }
 
-    public static String getBotToken() {
-        return botToken;
+    public TelegramService(String botToken, String botName) {
+        super(botToken);
+        this.botName = botName;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Long chatID = getChatId(update);
+        if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
+            this.sendMessage(chatID, "Ласкаво просимо. Цей бот допоможе відслідковувати актуальні курси валют.", Map.of());
+        }
+    }
+
+    public static Long getChatId(Update update) {
+        if (update.hasMessage()) return update.getMessage().getFrom().getId();
+        if (update.hasCallbackQuery()) return update.getCallbackQuery().getFrom().getId();
+        return null;
+    }
+
+    @Override
+    public int sendMessage(Long chatID, String text, Map<String, String> buttons) {
+        SendMessage message = new SendMessage();
+        message.setText(text);
+        message.setParseMode("markdown");
+        message.setChatId(chatID);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    @Override
+    public void deleteMessage(Long chatID, Map<Long, Integer> lastMessageIds) {
+
     }
 }
