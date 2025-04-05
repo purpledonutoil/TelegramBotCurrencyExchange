6fd138d0cc9dd2793fa8b1ff369480e0d4890f71
ideathreject <fury.extreme1@gmail.com>
2025-04-06 00:34:52 +0200

    getter added to Currency

--- src/main/java/banking/CurrencyRate.java
+++ src/main/java/banking/CurrencyRate.java
@@ -13,16 +13,32 @@ public class CurrencyRate {
         this.sellRate = sellRate;
     }
 
+    public Currency getCurrency() {
+        return currency;
+    }
+
     public void setCurrency(Currency currency) {
         this.currency = currency;
+    }
+
+    public Currency getBaseCurrency() {
+        return baseCurrency;
     }
 
     public void setBaseCurrency(Currency baseCurrency) {
         this.baseCurrency = baseCurrency;
     }
 
+    public float getBuyRate() {
+        return buyRate;
+    }
+
     public void setBuyRate(float buyRate) {
         this.buyRate = buyRate;
+    }
+
+    public float getSellRate() {
+        return sellRate;
     }
 
     public void setSellRate(float sellRate) {
