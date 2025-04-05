8f8ebf80678c8da6140fd687e2d9e7bdbf3f8d35
Yana <yanadavidenko999@gmail.com>
2025-04-05 16:45:31 +0200

    Storage

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -4,6 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage {
-    //               chatId, Settings
+
+    private static final Storage INSTANCE = new Storage();
+
     private final Map<Long, UserSettings> data = new HashMap<>();
+
+    private Storage() {
+    }
+
+    public static Storage getInstance() {
+        return INSTANCE;
+    }
+
+    public void saveUserSettings(long chatId, UserSettings settings) {
+        if (settings == null) return;
+        data.put(chatId, settings);
+    }
+
+    public UserSettings getUserSettings(long chatId) {
+        return data.computeIfAbsent(chatId, k -> new UserSettings());
+    }
 }
--- src/main/java/storage/UserSettings.java
+++ src/main/java/storage/UserSettings.java
@@ -1,9 +1,9 @@
 package storage;
-
 import banking.Bank;
 import banking.Currency;
 
 import java.util.EnumSet;
+import java.util.Objects;
 
 public class UserSettings {
     public static final Bank DEFAULT_BANK = Bank.PRIVAT;
@@ -15,4 +15,60 @@ public class UserSettings {
     private EnumSet<Currency> currencies;
     private int noticeTime;
     private int roundNumber;
+
+    public UserSettings() {
+        this.banks = EnumSet.of(DEFAULT_BANK);
+        this.currencies = EnumSet.of(DEFAULT_CURRENCY);
+        this.noticeTime = DEFAULT_NOTICE_TIME;
+        this.roundNumber = DEFAULT_ROUND_NUMBER;
+    }
+
+    public EnumSet<Bank> getBanks() {
+        return EnumSet.copyOf(banks);
+    }
+
+    public EnumSet<Currency> getCurrencies() {
+        return EnumSet.copyOf(currencies);
+    }
+
+    public int getNoticeTime() {
+        return noticeTime;
+    }
+
+    public int getRoundNumber() {
+        return roundNumber;
+    }
+
+
+    public int setNoticeTime(int noticeTime) {
+        this.noticeTime = noticeTime;
+        return this.noticeTime;
+    }
+
+    public int setRoundNumber(int roundNumber) {
+        this.roundNumber = roundNumber;
+        return this.roundNumber;
+    }
+
+    public void addCurrency(Currency currency) {
+        currencies.add(Objects.requireNonNull(currency));
+    }
+
+    public void removeCurrency(Currency currency) {
+        currencies.remove(currency);
+        if (currencies.isEmpty()) {
+            currencies.add(DEFAULT_CURRENCY);
+        }
+    }
+
+    public void addBank(Bank bank) {
+        banks.add(Objects.requireNonNull(bank));
+    }
+
+    public void removeBank(Bank bank) {
+        banks.remove(bank);
+        if (banks.isEmpty()) {
+            banks.add(DEFAULT_BANK);
+        }
+    }
 }
