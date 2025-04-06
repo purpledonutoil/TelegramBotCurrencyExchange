5842d319af721dece2bc99f5840cc16ae587e959
purpledonutoil <purpledonutoil@gmail.com>
2025-04-06 14:25:59 +0200

    Update UserSettings.java

--- src/main/java/storage/UserSettings.java
+++ src/main/java/storage/UserSettings.java
@@ -8,18 +8,18 @@ import java.util.Objects;
 public class UserSettings {
     public static final Bank DEFAULT_BANK = Bank.PRIVAT;
     public static final Currency DEFAULT_CURRENCY = Currency.USD;
-    public static final int DEFAULT_NOTICE_TIME = -1;
+    public static final int DEFAULT_NOTIFICATION_TIME = -1;
     public static final int DEFAULT_DECIMAL_PLACE = 2;
 
     private EnumSet<Bank> banks;
     private EnumSet<Currency> currencies;
-    private int  notificationTime;
+    private int notificationTime;
     private int decimalPlace;
 
     public UserSettings() {
         this.banks = EnumSet.of(DEFAULT_BANK);
         this.currencies = EnumSet.of(DEFAULT_CURRENCY);
-        this. notificationTime = DEFAULT_NOTICE_TIME;
+        this.notificationTime = DEFAULT_NOTIFICATION_TIME;
         this.decimalPlace = DEFAULT_DECIMAL_PLACE;
     }
 
@@ -32,7 +32,7 @@ public class UserSettings {
     }
 
     public int getNotificationTime() {
-        return  notificationTime;
+        return notificationTime;
     }
 
     public int getRoundNumber() {
@@ -40,8 +40,8 @@ public class UserSettings {
     }
 
 
-    public int setNotificationTime(int  notificationTime) {
-        this. notificationTime =  notificationTime;
+    public int setNotificationTime(int notificationTime) {
+        this.notificationTime = notificationTime;
         return this. notificationTime;
     }
 
