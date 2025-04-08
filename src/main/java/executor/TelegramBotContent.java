a205a22f9469165a2a7f9055fa68d1c103976ced
purpledonutoil <purpledonutoil@gmail.com>
2025-04-08 20:41:24 +0200

    Revert "bank-settings"

--- src/main/java/executor/TelegramBotContent.java
+++ src/main/java/executor/TelegramBotContent.java
@@ -6,18 +6,18 @@ import java.util.Map;
 public class TelegramBotContent {
     private TelegramBotContent(){
     }
-    protected static final String MESSAGE1 = "Ласкаво просимо. Цей бот допоможе відслідковувати актуальні курси валют.";
-    protected static final Map<String, String> BUTTONS1 = new LinkedHashMap<>();
-
+    protected static final String MESSAGE1 =
+            "Ласкаво просимо. Цей бот допоможе відслідковувати актуальні курси валют.";
+    protected static final Map<String, String> BUTTONS1 = Map.of(
+            "Отримати інфо", "info_btn",
+            "Налаштування", "settings_btn");
     protected static final String MESSAGE2 = "Налаштування:";
     protected static final Map<String, String> BUTTONS2 = new LinkedHashMap<>();
 
     protected static final String MESSAGE3 = "Оберіть кількість знаків після коми:";
     protected static final Map<String, String> BUTTONS3 = new LinkedHashMap<>();
-
     protected static final String MESSAGE4 = "Оберіть один або декілька банків:";
     protected static final Map<String, String> BUTTONS4 = new LinkedHashMap<>();
-
     protected static final String MESSAGE5 = "Оберіть одну або декілька валют:";
     protected static final Map<String, String> BUTTONS5 = new LinkedHashMap<>();
 
@@ -25,48 +25,38 @@ public class TelegramBotContent {
 
     protected static final Map<String, String> BUTTONS6 = new LinkedHashMap<>();
 
+    protected static final String MESSAGE7 = """
+            Курс в (БАНК): (ВАЛЮТА1/ВАЛЮТА2)
+            Покупка: ...
+            Продаж: ...""";
+
     static {
-        BUTTONS1.put("info_btn", "Отримати інфо");
-        BUTTONS1.put("settings_btn", "Налаштування");
+        BUTTONS2.put("Кількість знаків після коми", "decimalpoint_btn");
+        BUTTONS2.put("Банк", "bank_btn");
+        BUTTONS2.put("Валюти", "currency_btn");
+        BUTTONS2.put("Час оповіщень", "notification_btn");
 
-        BUTTONS2.put("decimalpoint_btn", "Кількість знаків після коми");
-        BUTTONS2.put("bank_btn", "Банк");
-        BUTTONS2.put("currency_btn", "Валюти");
-        BUTTONS2.put("notification_btn", "Час оповіщень");
+        BUTTONS3.put("2", "decimalpoint_btn1");
+        BUTTONS3.put("3", "decimalpoint_btn2");
+        BUTTONS3.put("4", "decimalpoint_btn3");
 
-        BUTTONS3.put("decimalpoint_btn1", "✅2");
-        BUTTONS3.put("decimalpoint_btn2", "3");
-        BUTTONS3.put("decimalpoint_btn3", "4");
+        BUTTONS4.put("НБУ", "bank_btn1");
+        BUTTONS4.put("ПриватБанк", "bank_btn2");
+        BUTTONS4.put("Монобанк", "bank_btn3");
 
-        BUTTONS4.put("bank_btn1", "НБУ");
-        BUTTONS4.put("bank_btn2", "✅ПриватБанк");
-        BUTTONS4.put("bank_btn3", "Монобанк");
+        BUTTONS5.put("USD", "currency_btn1");
+        BUTTONS5.put("EUR", "currency_btn2");
 
-        BUTTONS5.put("currency_btn1", "✅USD");
-        BUTTONS5.put("currency_btn2", "EUR");
-
-        BUTTONS6.put("notification_btn1", "9");
-        BUTTONS6.put("notification_btn2", "10");
-        BUTTONS6.put("notification_btn3", "11");
-        BUTTONS6.put("notification_btn4", "12");
-        BUTTONS6.put("notification_btn5", "13");
-        BUTTONS6.put("notification_btn6", "14");
-        BUTTONS6.put("notification_btn7", "15");
-        BUTTONS6.put("notification_btn8", "16");
-        BUTTONS6.put("notification_btn9", "17");
-        BUTTONS6.put("notification_btn10", "18");
-        BUTTONS6.put("notification_btn11", "Вимкнути повідомлення");
-
-//        BUTTONS6.put("9", "notification_btn1");
-//        BUTTONS6.put("10", "notification_btn2");
-//        BUTTONS6.put("11", "notification_btn3");
-//        BUTTONS6.put("12", "notification_btn4");
-//        BUTTONS6.put("13", "notification_btn5");
-//        BUTTONS6.put("14", "notification_btn6");
-//        BUTTONS6.put("15", "notification_btn7");
-//        BUTTONS6.put("16", "notification_btn8");
-//        BUTTONS6.put("17", "notification_btn9");
-//        BUTTONS6.put("18", "notification_btn10");
-//        BUTTONS6.put("Вимкнути повідомлення", "notification_btn11");
+        BUTTONS6.put("9", "notification_btn1");
+        BUTTONS6.put("10", "notification_btn2");
+        BUTTONS6.put("11", "notification_btn3");
+        BUTTONS6.put("12", "notification_btn4");
+        BUTTONS6.put("13", "notification_btn5");
+        BUTTONS6.put("14", "notification_btn6");
+        BUTTONS6.put("15", "notification_btn7");
+        BUTTONS6.put("16", "notification_btn8");
+        BUTTONS6.put("17", "notification_btn9");
+        BUTTONS6.put("18", "notification_btn10");
+        BUTTONS6.put("Вимкнути повідомлення", "notification_btn11");
     }
 }
--- src/main/java/executor/commands/TelegramBotUtils.java
+++ src/main/java/executor/commands/TelegramBotUtils.java
@@ -1,15 +1,14 @@
 package executor.commands;
 
-import banking.Currency;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class TelegramBotUtils {
     private TelegramBotUtils() {
@@ -49,50 +48,14 @@ public class TelegramBotUtils {
 
         for (Map.Entry<String, String> entry : buttons.entrySet()) {
             InlineKeyboardButton button = new InlineKeyboardButton();
-            button.setText(entry.getValue());
-            button.setCallbackData(entry.getKey());
+            button.setText(entry.getKey());
+            button.setCallbackData(entry.getValue());
             keyboard.add(List.of(button));
         }
 
         markup.setKeyboard(keyboard);
         message.setReplyMarkup(markup);
     }
-
-    public static EditMessageReplyMarkup modifyButtons(Long chatId, int messageId, String[] values, Map<String, String> buttons) {
-        EditMessageReplyMarkup editMarkup = new EditMessageReplyMarkup();
-        editMarkup.setChatId(chatId);
-        editMarkup.setMessageId(messageId);
-        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
-
-        for (Map.Entry<String, String> entry : buttons.entrySet()) {
-            if (entry.getValue().contains("✅")){
-                buttons.replace(entry.getKey(), entry.getValue().substring(1));
-            }
-        }
-        for (String value : values) {
-            for (Map.Entry<String, String> entry : buttons.entrySet()) {
-                if (entry.getValue().equals(value)){
-                    buttons.replace(entry.getKey(), "✅" + entry.getValue());
-                }
-            }
-        }
-
-        for (Map.Entry<String, String> entry : buttons.entrySet()) {
-            InlineKeyboardButton newButton = new InlineKeyboardButton();
-            newButton.setText(entry.getValue());
-            newButton.setCallbackData(entry.getKey());
-            List<InlineKeyboardButton> row = new ArrayList<>();
-            row.add(newButton);
-            keyboard.add(List.of(newButton));
-        }
-
-        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
-        markup.setKeyboard(keyboard);
-        editMarkup.setReplyMarkup(markup);
-
-        return editMarkup;
-    }
-
 
     private static void attachKeyboard(SendMessage message, Map<String, String> buttons) {
         ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
@@ -101,7 +64,7 @@ public class TelegramBotUtils {
         KeyboardRow row = new KeyboardRow();
         int count = 0;
         for (Map.Entry<String, String> entry : buttons.entrySet()) {
-            row.add(entry.getValue());
+            row.add(entry.getKey());
             count++;
             if (count%3==0){
                 keyboard.add(row);
